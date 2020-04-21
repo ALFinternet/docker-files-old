2bc1bd9439614f1ba668c4f785ea6159e297e3bd
ALFinternet <ALFinternet@gmail.com>
2020-04-21 13:13:22 -0700

    comma fix

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -2,7 +2,6 @@
 # https://github.com/linuxserver/docker-letsencrypt/blob/master/README.md
 # https://blog.linuxserver.io/2017/11/28/how-to-setup-a-reverse-proxy-with-letsencrypt-ssl-for-all-your-docker-apps/
 
-
 docker create \
   --name=letsencrypt \
   --cap-add=NET_ADMIN \
@@ -10,7 +9,7 @@ docker create \
   -e PGID=1000 \
   -e TZ=America/Los_Angeles \
   -e URL=home.finchtech.com \
-  -e SUBDOMAINS=wildcard, \
+  -e SUBDOMAINS=wildcard \
   -e VALIDATION=http \
   -e DNSPLUGIN=cloudflare `#optional` \
   -e DUCKDNSTOKEN=<token> `#optional` \
