d3e38ab2c300b01e4be8670c434139d4b3674600
ALFinternet <ALFinternet@gmail.com>
2020-04-24 14:28:36 -0700

    add organizr

---
+++ organizr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker create \
+  --name=organizr \
+  --net=docker_bridge \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=America/Los_Angeles \
+  -p 9983:80 \
+  -v path to data:/config \
+  -v /mnt/hd2/docker-vols/organizr_config:/config \
+  --restart unless-stopped \
+  linuxserver/organizr
